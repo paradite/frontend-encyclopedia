5629cea3e53cfba6582027011a065f0d5d525f91
Zhu Liang <zhuliangg11@gmail.com>
2023-08-06 14:10:09 +0800

    feat: modify script

--- generateReadme.js
+++ generateReadme.js
@@ -7,9 +7,9 @@ let content = '# Frontend Encyclopedia\n\n';
 for (const key in data) {
   content += `### ${key}\n`;
   const terms = data[key].map((term) =>
-    term.url ? `[${term.name}](${term.url})` : term.name
+    term.url ? `- [${term.name}](${term.url})` : `- ${term.name}`
   );
-  content += terms.join(', ') + '\n\n';
+  content += terms.join('\n') + '\n\n';
 }
 
 fs.writeFileSync('README.md', content);
