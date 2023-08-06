3405318250f5e71616878d19bf32b69bef5a278f
Zhu Liang <zhuliangg11@gmail.com>
2023-08-06 14:09:01 +0800

    feat: update script

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# frontend-encyclopedia
-Frontend Encyclopedia
+# Frontend Encyclopedia
+
+A: AJAX, Angular, Axios
+B: Babel, Bootstrap
+C: Chakra-UI, CSS3, Cypress
+D: D3.js, Docker
+E: ES2022, Express.js
+F: Figma, Framer
+G: Gatsby, GraphQL
+H: HTML5
+I: Immutable.js
+J: Jest, JavaScript
+K: Keyframes
+L: Less, lodash
+M: Material-UI
+N: Next.js, npm
+O: OAuth
+P: Parcel, PWA
+Q: Quasar
+R: React, Redux, REST, RxJS
+S: SCSS, Styled-components, Svelte
+T: TailwindCSS, TypeScript
+U: Underscore.js
+V: Vue.js
+W: Webpack
+X: XState
+Y: Yarn
+Z: Z-index
--- generateReadme.js
+++ generateReadme.js
@@ -2,11 +2,14 @@ const fs = require('fs');
 
 const data = require('./data.json');
 
-let content = "# Frontend Encyclopedia\n\n";
+let content = '# Frontend Encyclopedia\n\n';
 
 for (const key in data) {
-  const terms = data[key].map(term => term.url ? `[${term.name}](${term.url})` : term.name);
-  content += key + ': ' + terms.join(', ') + '\n';
+  content += `### ${key}\n`;
+  const terms = data[key].map((term) =>
+    term.url ? `[${term.name}](${term.url})` : term.name
+  );
+  content += terms.join(', ') + '\n\n';
 }
 
 fs.writeFileSync('README.md', content);
